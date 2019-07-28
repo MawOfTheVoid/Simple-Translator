d73d6a39d06a52fab32ce75f84405cb025caefe7
The Void <MawOfTheVoid@protonmail.com>
2019-07-28 23:47:35 +0200

    They are back again

---
+++ popup/translate.js
@@ -0,0 +1,96 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    response = xmlHttp.responseText
+    delete xmlHttp
+    return response;
+}
+
+function clean_translation(txt)
+{
+  txt = txt.replace(/\[/g, "");
+  txt = txt.replace(/\]/g, "");
+  txt = txt.replace(/\"/g, "");
+  /**
+  txt = txt.replace(/null/g, "");
+  txt = txt.replace(/1/g, "");
+  txt = txt.replace(/\"/g, "");
+  */
+  txt = txt.split(",");
+  translation = txt[0]
+  if (translation === "null")
+  {
+    translation = "Please enter a word to translate!"
+  }
+  return translation
+}
+
+
+function get_language()
+{
+  var radios = document.getElementsByName('language');
+  var language;
+
+for (var i = 0, length = radios.length; i < length; i++)
+  {
+    if (radios[i].checked)
+    {
+      language = radios[i].id;
+      break;
+    }
+  }
+  return language
+}
+
+function write_output(answer)
+{
+  var p = document.getElementById("answer");
+  p.textContent = answer
+}
+
+function translate_click() {
+  // en=english german=de spanish=es
+  var text = document.getElementById('textinput').value;
+  var lan = get_language()
+  var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + "auto" + "&tl=" + lan + "&dt=t&q=" + encodeURI(text);
+  var txt = httpGet(url);
+  var translation = clean_translation(txt);
+  write_output(translation)
+}
+
+function init_addon()
+{
+  document.getElementById("btn").addEventListener("click", function(){translate_click();});
+  document.getElementById("de").addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      translate_click();
+    }
+  });
+
+  document.getElementById("en").addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      translate_click();
+    }
+  });
+
+  document.getElementById("es").addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      translate_click();
+    }
+  });
+
+  document.getElementById("textinput").addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      translate_click();
+    }
+  });
+}
+
+
+init_addon()
